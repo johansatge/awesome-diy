5eb5c7659eacf3b65972faf806296d3928b346d8
Johan Satgé <johansatge@users.noreply.github.com>
2022-04-22 18:29:46 +0200

    Initial commit

---
+++ scripts/build-markdown.js
@@ -0,0 +1,46 @@
+/**
+ * Generate a markdown table from the local channels.json file
+ */
+
+const fsp = require('fs').promises
+const path = require('path')
+
+const channelsPath = path.join(__dirname, '../channels.json')
+
+try {
+  run()
+} catch(error) {
+  console.log(`Run error: ${error.message}`)
+}
+
+async function run() {
+  const channelsJson = JSON.parse(await fsp.readFile(channelsPath, 'utf8'))
+  const markdown = []
+  markdown.push('📷|Channel|Description')
+  markdown.push('---|---|---')
+  for (const channel of channelsJson.channels) {
+    const country = channel.country.length > 0 ? `(${channel.country})` : ''
+    const line = [
+      `<img src="${channel.thumbnail}" width="100px" alt="${channel.id}">`,
+      `[${channel.name}](https://www.youtube.com/channel/${channel.id}) ${country}`,
+      descriptionMarkup(channel.description),
+    ]
+    markdown.push(line.join('|'))
+  }
+  console.log(markdown.join('\n'))
+}
+
+function descriptionMarkup(rawDescription) {
+  const maxLength = 120
+  const oneLine = (text) => text.replaceAll('\n', ' ')
+  if (rawDescription.length === 0) {
+    return '_No description_'
+  }
+  if (rawDescription.length <= maxLength) {
+    return oneLine(rawDescription)
+  }
+  const space = rawDescription.indexOf(' ', maxLength)
+  const summary = rawDescription.substring(0, space)
+  const rest = rawDescription.substring(space + 1)
+  return `<details><summary>${oneLine(summary)} [...]</summary>${oneLine(rest)}</details>`
+}
---
+++ scripts/consolidate-json.js
@@ -0,0 +1,68 @@
+/**
+ * - Fetch fresh YouTube data for each channel in channels.json
+ * - Update channel name, description & thumbnail
+ * - Reorder channels in alphabetical order
+ */
+
+const https = require('https')
+const fsp = require('fs').promises
+const path = require('path')
+const { apiKey } = require('../.apikey.js')
+
+const channelsPath = path.join(__dirname, '../channels.json')
+
+try {
+  run()
+} catch(error) {
+  console.log(`Run error: ${error.message}`)
+}
+
+async function run() {
+  const channelsJson = JSON.parse(await fsp.readFile(channelsPath, 'utf8'))
+  const consolidatedChannels = []
+  for (const channel of channelsJson.channels) {
+    const freshChannelData = await fetchChannelData(channel.id)
+    channel.thumbnail = freshChannelData.snippet.thumbnails.medium.url
+    channel.name = freshChannelData.snippet.localized.title
+    channel.description = freshChannelData.snippet.localized.description
+    channel.country = freshChannelData.snippet.country || ''
+    consolidatedChannels.push(channel)
+  }
+  consolidatedChannels.sort((a, b) => a.name > b.name ? 1 : (a.name < b.name ? -1 : 0))
+  await fsp.writeFile(channelsPath, JSON.stringify({
+    lastupdate: new Date().toString(),
+    channels: consolidatedChannels,
+  }, null, 2), 'utf8')
+}
+
+function fetchChannelData(channelId) {
+  const options = {
+    hostname: 'www.googleapis.com',
+    path: `/youtube/v3/channels?key=${apiKey}&id=${channelId}&part=snippet&maxResults=1`,
+    method: 'GET',
+  }
+  return new Promise((resolve, reject) => {
+    const request = https.request(options, (response) => {
+      let data = ''
+      response.on('data', (chunk) => {
+        data += chunk
+      })
+      response.on('end', () => {
+        try {
+          const json = JSON.parse(data)
+          if (!json.items || json.items.length === 0) {
+            reject(new Error(`Channel not found in response`))
+            return
+          }
+          resolve(json.items[0])
+        } catch(error) {
+          reject(new Error(`Could not parse response (${error.message})`))
+        }
+      })
+    })
+    request.on('error', error => {
+      reject(error)
+    })
+    request.end()
+  })
+}
